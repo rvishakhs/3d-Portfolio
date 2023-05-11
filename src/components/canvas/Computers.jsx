12cbd76102d23a6204f2f9c08ff8a719558869eb
rvishakhs <rvishakhs@gmail.com>
2023-05-12 00:29:50 +0100

    working on three js

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,12 +2,35 @@ import {Suspense, useEffect, useState } from 'react'
 
 import { Canvas } from '@react-three/fiber'
 
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+
+import CanvasLoader from '../Loader';
+
 
 const Computers = () => {
-  return (
-    <div>
 
-    </div>
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+  return (
+    <mesh>
+        <hemisphereLight intensity={0.15} groundColor="black"/>
+        <pointLight intensity={1}/>
+        
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows="true"
+      camera={{position : [20, 3, 5], fov: 25}}
+      gl={{preserveDrawingBuffer : true}}
+    > 
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false}/>
+      </Suspense>   
+    </Canvas>
   )
 }
 
