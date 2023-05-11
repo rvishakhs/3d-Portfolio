728097932df16f645b8edbaa54df7aa4eb12c074
rvishakhs <rvishakhs@gmail.com>
2023-05-11 23:54:23 +0100

    Creating 3d model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,11 +14,13 @@ const Hero = () => {
             <div  className='w-5 h-5 rounded-full bg-[#915eff]'/>
             <div className='w-1 sm:h-80 h-40 violet-gradient '/>
           </div>
-          <div className='p-2 mt-5 flex flex-col items-start justify-center'>
-            <h1 className={`${styles.heroHeadText} text-white`}>  Hi I'm <span className='text-[#915eff]'>Visakh </span> </h1>
+          <div className='p-2 mt-5 flex flex-col items-start justify-center gap-2'>
+            <h1 className={`${styles.heroHeadText} text-white`}>  Hi I'm <span className='text-[#915eff] tracking-wide'>Visakh </span> </h1>
+            <p className={`${styles.heroSubText}`}>Passionate MERN stack developer <br/> empowering dynamic web experiences <br/> from front to back.</p>
 
           </div>
       </div>
+          <ComputersCanvas />
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import {Suspense, useEffect, useState } from 'react'
+
+import { Canvas } from '@react-three/fiber'
+
 
 const Computers = () => {
   return (
-    <div>Computers</div>
+    <div>
+
+    </div>
   )
 }
 
