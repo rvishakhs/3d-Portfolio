8d61683b89e5077a78827016252d87d8d7c62df5
rvishakhs <rvishakhs@gmail.com>
2023-05-17 22:24:02 +0100

    working on git push

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,6 +21,22 @@ const Hero = () => {
           </div>
       </div>
           <ComputersCanvas />
+          <div className='absolute xs:bottom-10 bottom-32 w-full flex items-center justify-center'>
+            <a href='#about'>
+              <div 
+                className='w-[28px] h-[48px] rounded-3xl border-4 border-secondary justify-center flex items-start p-2 hover:border-white'>
+                <motion.dev 
+                  animate={{ y :[0, 24, 0]}}
+                  transition={{
+                    duration: 1.5,
+                    repeat : Infinity,
+                    repeatType: 'loop',
+                  }}
+                  className="w-3, h-3 rounded-full bg-secondary mb-1"  
+                />
+              </div>
+            </a>
+          </div>
     </section>
   )
 }
