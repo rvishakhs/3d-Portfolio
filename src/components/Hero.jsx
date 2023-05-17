3e222cb46b12b4447ef820b3ea668c7b713e8759
rvishakhs <rvishakhs@gmail.com>
2023-05-17 22:34:11 +0100

    barking

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,15 +24,17 @@ const Hero = () => {
           <div className='absolute xs:bottom-10 bottom-32 w-full flex items-center justify-center'>
             <a href='#about'>
               <div 
-                className='w-[28px] h-[48px] rounded-3xl border-4 border-secondary justify-center flex items-start p-2 hover:border-white'>
+                className='w-[32px] h-[52px] rounded-3xl border-4 border-secondary justify-center flex items-start p-2 hover:border-white'>
                 <motion.dev 
-                  animate={{ y :[0, 24, 0]}}
+                  animate={{ 
+                    y : [0, 14, 0]
+                  }}
                   transition={{
                     duration: 1.5,
                     repeat : Infinity,
                     repeatType: 'loop',
                   }}
-                  className="w-3, h-3 rounded-full bg-secondary mb-1"  
+                  className="w-3 h-3 rounded-full bg-secondary mb-1 hover:bg-white"  
                 />
               </div>
             </a>
