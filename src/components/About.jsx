d808d1a607e501dc6e3f3869f3e8b4c991f18ec9
rvishakhs <rvishakhs@gmail.com>
2023-05-21 00:51:59 +0100

    Hai everybody

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+
+import { styles } from '../styles'
+import { services } from '../constant' 
+import { fadeIn, textVariant } from '../uttils/motion'
+
+const Service = ({index, title, icon}) => {
+  return (
+      <Tilt className="xs:w-[250px] w-full">
+        <motion.div
+          variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+          className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+        >
+          <div
+            options={{
+              max: 45,
+              scale: 1,
+              speed: 450
+            }}
+          >
+
+          </div>
+        </motion.div>
+      </Tilt>
+  )
+}
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div
+        variants={textVariant()}
+      >
+        <p className={styles.sectionSubText}>
+          INTRODUCTION
+        </p>
+        <h1 className={styles.heroHeadText}>
+          OverView
+        </h1>
+      </motion.div>
+
+      <motion.p 
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] text-justify'
+      >
+      Highly skilled and versatile DevOps & MERN Stack Developer with a strong passion for accelerating software development. Adept at bridging the gap between development and operations teams, leveraging the power of DevOps principles and expertise in the MERN stack (MongoDB, Express.js, React.js, Node.js) to streamline processes and deliver efficient, high-quality web applications. Experienced in configuring infrastructure, automating deployment, implementing robust testing frameworks, and optimizing workflows for agile, iterative development. Collaborative and detail-oriented, committed to delivering seamless user experiences and driving continuous improvement in software development practices.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <Service key={service.title} index={index} {...service} />
+        ) )}
+
+      </div>
+    </>
   )
 }
 
